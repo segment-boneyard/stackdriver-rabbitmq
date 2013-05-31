e2b0d694f242d3b12208bb86545ad7ebefbb2344
Peter Reinhardt <peter@segment.io>
2013-05-31 01:04:42 -0700

    scaffolded out

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-var _        = require('underscore')
-  , CronJob  = require('cron').CronJob
-  , rabbitmq = require('rabbitmq-monitor');
+var _           = require('underscore')
+  , CronJob     = require('cron').CronJob
+  , rabbitmq    = require('rabbitmq-monitor')
+  , Stackdriver = require('stackdriver-custom')
+  , conf        = require('./conf.js');
 
-// TODO
-// create a chron job that runs every minute
-// use rabbitmq to get whatever stats desired
-// send each stat to stackdriver
-// http://feedback.stackdriver.com/knowledgebase/articles/181488-sending-custom-metrics-to-the-stackdriver-system
+var stackdriver = new Stackdriver(conf);
+
+var collectMetrics = function () {
+
+    rabbitmq.listQueues(conf.virtualHostPath, 'messages name', function (queues) {
+        var sum = _.reduce(queues, function (memo, queue) {
+            return (memo + queue.messages);
+        }, 0);
+
+        sum += Math.random() * 100;
+        stackdriver.sendMetric('test random messages count', sum);
+    });
+};
+
+
+// execute on a chron
+new CronJob({
+    cronTime : '* * * * * *', // runs every minute
+    onTick   : collectMetrics,
+    start    : true,
+    timeZone : 'America/Los_Angeles'
+});
+
